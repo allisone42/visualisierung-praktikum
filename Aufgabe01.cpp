d9fe89f5294391b8a03b6fca7890489d3d2b5a46
allisone42 <33097879+allisone42@users.noreply.github.com>
2017-10-25 21:51:13 +0200

    Update Aufgabe01.cpp

--- Aufgabe01.cpp
+++ Aufgabe01.cpp
@@ -36,26 +36,49 @@ namespace
 
         void execute( const Algorithm::Options& options, const volatile bool& /*abortFlag*/ ) override
         {
-            Tensor<double, 3> p0(1.0, 0.0, 0.0);
-            Tensor<double, 3> p1(0.0, 1.0, 0.0);
-            Tensor<double, 3> p2(0.0, 0.0, 1.0);
+            //int numberOfPoints = 8;
+            //int tensorField[numberOfPoints];
 
-            std::vector<Tensor<double,3>> myTriangle;
-            myTriangle.push_back(p0);
-            myTriangle.push_back(p1);
-            myTriangle.push_back(p2);
+            // points for hexahedron
+            // floor
+            Tensor<double, 3> p0(0.0, 0.0, 0.0);
+            Tensor<double, 3> p1(1.0, 0.0, 0.0);
+            Tensor<double, 3> p2(1.0, 1.0, 0.0);
+            Tensor<double, 3> p3(0.0, 1.0, 0.0);
+            // level
+            Tensor<double, 3> p4(0.0, 1.0, 1.0);
+            Tensor<double, 3> p5(1.0, 1.0, 1.0);
+            Tensor<double, 3> p6(1.0, 0.0, 1.0);
+            Tensor<double, 3> p7(0.0, 0.0, 1.0);
 
-            for (std::vector<Tensor<double,3 > >::const_iterator i = myTriangle.begin(); i != myTriangle.end(); ++i)
-                debugLog() << *i << ' ' ; // shows me the tensors
+            // roof
+            Tensor<double, 3> p8(0.5, 0.5, 1.5);
 
-            std::shared_ptr< const DiscreteDomain< 3 > > myDomain = DomainFactory::makeDomainArbitrary(std::move(myTriangle), Precision::UINT64);
 
-            std::vector< size_t > pointsToDraw({0,1,2});
+            std::vector<Tensor<double, 3>> myHexahedron;
+
+            myHexahedron.push_back(p0);
+            myHexahedron.push_back(p1);
+            myHexahedron.push_back(p2);
+            myHexahedron.push_back(p3);
+            myHexahedron.push_back(p4);
+            myHexahedron.push_back(p5);
+            myHexahedron.push_back(p6);
+            myHexahedron.push_back(p7);
+            myHexahedron.push_back(p8);
+
+            //for (std::vector<Tensor<double,3 > >::const_iterator i = myHexahedron.begin(); i != myHexahedron.end(); ++i)
+            //    debugLog() << *i << ' ' ; // shows me the tensors
+
+            std::shared_ptr< const DiscreteDomain< 3 > > myDomain = DomainFactory::makeDomainArbitrary(std::move(myHexahedron), Precision::UINT64);
+
+            std::vector< size_t > pointsToDraw({0,1,2,3,4,5,6,7, 7,6,5,4,8});
 
             std::pair<Cell::Type, size_t> cellCounts[1];
-            cellCounts[0] = std::pair<Cell::Type, size_t> (Cell::TRIANGLE,1);
+            cellCounts[0] = std::pair<Cell::Type, size_t> (Cell::HEXAHEDRON, 1);
+            cellCounts[1] = std::pair<Cell::Type, size_t> (Cell::PYRAMID, 1);
 
-            std::shared_ptr< const Grid< 3 > > myGrid = DomainFactory::makeGridUnstructured( *myDomain, 1, cellCounts, pointsToDraw);
+            std::shared_ptr< const Grid< 3 > > myGrid = DomainFactory::makeGridUnstructured( *myDomain, 2, cellCounts, pointsToDraw);
             setResult("grid", myGrid);
 
             debugLog() << std::endl;
