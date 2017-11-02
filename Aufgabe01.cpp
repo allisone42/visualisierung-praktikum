750d8970faa6b05e3443928631988f6ab4554da7
allisone42 <33097879+allisone42@users.noreply.github.com>
2017-11-02 19:40:29 +0100

    Aufgabe 1 working

--- Aufgabe01.cpp
+++ Aufgabe01.cpp
@@ -12,84 +12,149 @@ namespace
     {
         //bool isAutoRun = true; //reloads options when changes occur
 
-    public:
-        struct Options : public DataAlgorithm::Options
-        {
-            Options( fantom::Options::Control& control ) : DataAlgorithm::Options(control)
+        public:
+            struct Options : public DataAlgorithm::Options
             {
-                add< int >("Number of houses", "Specify how many houses you want", 1);
+                Options(fantom::Options::Control & control)
+                    : DataAlgorithm::Options(control)
+                {
+                    addTab("Basic");
+                    add<int>("Number of houses", "Specify how many houses you want (at least 4)", 4, &acceptNumberOfHouses);
+                }
+
+                static int acceptNumberOfHouses( const int& i )
+                {
+                    int number = std::max(i, 0);
+
+                    // at least 4 houses
+                    if (number >= 4)
+                        return number;
+                    else return 4;
+                }
+            };
+
+            struct DataOutputs : public DataAlgorithm::DataOutputs
+            {
+                DataOutputs(fantom::DataOutputs::Control& control) : DataAlgorithm::DataOutputs(control)
+                {
+                    add<Grid<3>>("grid");
+                }
+            };
+
+            Aufgabe01(InitData & data)
+                : DataAlgorithm(data)
+            {
+                // initialize internal data members
+            }
+
+            void execute(const Algorithm::Options& options, const volatile bool& /*abortFlag*/) override
+            {
+                int numberOfHouses = options.get< int >("Number of houses");
+                const double DISTANCE = 3.0;
+                const size_t NUMBEROFCELLTYPES = 4;
+
+                // vector consisting of data points
+                std::vector<Tensor<double, 3>> points;
+                // indices of points
+                std::vector<size_t> indices;
+
+                // create levels (Hexadedrons)
+                for (int i = 0; i < numberOfHouses; i++) {
+                    int shift = i * DISTANCE;
+
+                    // floor
+                    Tensor<double, 3> p0(0.0, 0.0 + shift, 0.0);
+                    Tensor<double, 3> p1(1.0, 0.0 + shift, 0.0);
+                    Tensor<double, 3> p2(1.0, 1.0 + shift, 0.0);
+                    Tensor<double, 3> p3(0.0, 1.0 + shift, 0.0);
+
+                    // level
+                    Tensor<double, 3> p4(0.0, 1.0 + shift, 1.0);
+                    Tensor<double, 3> p5(1.0, 1.0 + shift, 1.0);
+                    Tensor<double, 3> p6(1.0, 0.0 + shift, 1.0);
+                    Tensor<double, 3> p7(0.0, 0.0 + shift, 1.0);
+
+                    points.push_back(p0);
+                    points.push_back(p1);
+                    points.push_back(p2);
+                    points.push_back(p3);
+                    points.push_back(p4);
+                    points.push_back(p5);
+                    points.push_back(p6);
+                    points.push_back(p7);
+
+                    for(int j = 0; j < 8; j++) {
+                        indices.push_back(j + (i * 8));
+                    }
+                }
+
+                // roofs (pyramids)
+                for (int i = 0; i < numberOfHouses; i++) {
+                    int shift = i * DISTANCE;
+
+                    Tensor<double, 3> p8(0.5, 0.5 + shift, 1.5);
+
+                    points.push_back(p8);
+
+                    for(int j = 4 + (i * 8); j < (8 + (i * 8)); j++) {
+                        indices.push_back(j);
+                    }
+                    indices.push_back(8 * numberOfHouses + i);
+                }
+
+                // poles (lines)
+                for (int i = 0; i < numberOfHouses; i++) {
+                    int shift = i * DISTANCE;
+
+                    Tensor<double, 3> p9(0.5, 0.5 + shift, 1.7);
+
+                    points.push_back(p9);
+
+                    indices.push_back(8 * numberOfHouses + i);
+                    indices.push_back(9 * numberOfHouses + i);
+                }
+
+                // flags (triangles)
+                for (int i = 0; i < numberOfHouses; i++) {
+                    int shift = i * DISTANCE;
+
+                    Tensor<double, 3> p10(0.5, 0.5 + shift, 1.6);
+                    Tensor<double, 3> p11(0.5, 0.7 + shift, 1.66);
+
+                    points.push_back(p10);
+                    points.push_back(p11);
+
+                    indices.push_back(9 * numberOfHouses + i);
+                    indices.push_back(points.size() - 2);
+                    indices.push_back(points.size() - 1);
+                }
+
+                std::pair<Cell::Type, size_t> cellCounts[NUMBEROFCELLTYPES];
+                size_t houses = (size_t) numberOfHouses;
+
+                std::pair<Cell::Type, size_t> hexas(Cell::HEXAHEDRON, houses);
+                std::pair<Cell::Type, size_t> pyramids(Cell::PYRAMID, houses);
+                std::pair<Cell::Type, size_t> lines(Cell::LINE, houses);
+                std::pair<Cell::Type, size_t> triangles(Cell::TRIANGLE, houses);
+
+                cellCounts[0] = hexas;
+                cellCounts[1] = pyramids;
+                cellCounts[2] = lines;
+                cellCounts[3] = triangles;
+
+//                for (std::vector<size_t>::const_iterator i = indices.begin(); i != indices.end(); ++i)
+//                    std::cout << *i << ' ' ; // log tensors
+
+                // create 3 dimensional Domain
+                std::shared_ptr< const DiscreteDomain< 3 > > myDomain = DomainFactory::makeDomainArbitrary(points, Precision::UINT64);
+
+                // create Grid and fill it with domain, all data points and indices
+                std::shared_ptr< const Grid< 3 > > myGrid = DomainFactory::makeGridUnstructured( *myDomain, NUMBEROFCELLTYPES, cellCounts, indices);
+
+                // show grid
+                setResult("grid", myGrid);
             }
         };
 
-        struct DataOutputs : public DataAlgorithm::DataOutputs
-        {
-            DataOutputs(fantom::DataOutputs::Control& control) : DataAlgorithm::DataOutputs(control)
-            {
-                add< Grid< 3 > >("grid");
-            }
-        };
-
-        Aufgabe01( InitData& data ) : DataAlgorithm( data )
-        {
-            //initialize internal data members
-        }
-
-        void execute( const Algorithm::Options& options, const volatile bool& /*abortFlag*/ ) override
-        {
-            size_t numberOfCells = 4;
-
-            // floor
-            Tensor<double, 3> p0(0.0, 0.0, 0.0);
-            Tensor<double, 3> p1(1.0, 0.0, 0.0);
-            Tensor<double, 3> p2(1.0, 1.0, 0.0);
-            Tensor<double, 3> p3(0.0, 1.0, 0.0);
-            // level
-            Tensor<double, 3> p4(0.0, 1.0, 1.0);
-            Tensor<double, 3> p5(1.0, 1.0, 1.0);
-            Tensor<double, 3> p6(1.0, 0.0, 1.0);
-            Tensor<double, 3> p7(0.0, 0.0, 1.0);
-            // roof
-            Tensor<double, 3> p8(0.5, 0.5, 1.5);
-            // flag
-            Tensor<double, 3> p9(0.5, 0.5, 1.7);
-            Tensor<double, 3> p10(0.5, 0.7, 1.66);
-            Tensor<double, 3> p11(0.5, 0.5, 1.6);
-
-            std::vector<Tensor<double, 3>> myHouse;
-
-            myHouse.push_back(p0);
-            myHouse.push_back(p1);
-            myHouse.push_back(p2);
-            myHouse.push_back(p3);
-            myHouse.push_back(p4);
-            myHouse.push_back(p5);
-            myHouse.push_back(p6);
-            myHouse.push_back(p7);
-            myHouse.push_back(p8);
-            myHouse.push_back(p9);
-            myHouse.push_back(p10);
-            myHouse.push_back(p11);
-
-            for (std::vector<Tensor<double,3 > >::const_iterator i = myHouse.begin(); i != myHouse.end(); ++i)
-                debugLog() << *i << ' ' ; // shows me the tensors
-
-            std::shared_ptr< const DiscreteDomain< 3 > > myDomain = DomainFactory::makeDomainArbitrary(std::move(myHouse), Precision::UINT64);
-
-            std::vector< size_t > pointsToDraw({0,1,2,3,4,5,6,7, 7,6,5,4,8, 8,9, 9,11,10});
-
-            std::pair<Cell::Type, size_t> cellCounts[numberOfCells];
-
-            cellCounts[0] = std::pair<Cell::Type, size_t> (Cell::HEXAHEDRON, 1);
-            cellCounts[1] = std::pair<Cell::Type, size_t> (Cell::PYRAMID, 1);
-            cellCounts[2] = std::pair<Cell::Type, size_t> (Cell::LINE, 1);
-            cellCounts[3] = std::pair<Cell::Type, size_t> (Cell::TRIANGLE, 1);
-
-            std::shared_ptr< const Grid< 3 > > myGrid = DomainFactory::makeGridUnstructured( *myDomain, numberOfCells, cellCounts, pointsToDraw);
-            setResult("grid", myGrid);
-
-            debugLog() << std::endl;
-
-        }
-    };
     AlgorithmRegister< Aufgabe01 > dummy( "VisPraktikum/Aufgabe01", "Tiny Houses." );
 }
