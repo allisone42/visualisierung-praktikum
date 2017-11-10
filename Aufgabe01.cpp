3aaa85f8719864756868a11afc195b6436547273
allisone42 <33097879+allisone42@users.noreply.github.com>
2017-11-10 16:35:17 +0100

    Refactor Aufgabe01

--- Aufgabe01.cpp
+++ Aufgabe01.cpp
@@ -19,14 +19,14 @@ namespace
                     : DataAlgorithm::Options(control)
                 {
                     addTab("Basic");
-                    add<int>("Number of houses", "Specify how many houses you want (at least 4)", 4, &acceptNumberOfHouses);
+                    add<int>("Number of houses", "Specify how many houses you want (at least 4)", 4, &getNumberOfHouses);
                 }
 
-                static int acceptNumberOfHouses( const int& i )
+                static int getNumberOfHouses( const int& i )
                 {
                     int number = std::max(i, 0);
 
-                    //check if number typed in was at least 4
+                    //check if number typed in was at least 4, if not, set 4
                     if (number >= 4)
                         return number;
                     else {
@@ -54,9 +54,6 @@ namespace
                 //getting number of houses from users options
                 int numberOfHouses = options.get< int >("Number of houses");
 
-                //distance between the houses
-                const double DISTANCE = 3.0;
-
                 //cell types are hexadedron, pyramid, line, triangle
                 const size_t NUMBEROFCELLTYPES = 4;
 
@@ -68,8 +65,8 @@ namespace
 
                 //create levels (hexadedrons)
                 for (int i = 0; i < numberOfHouses; i++) {
-                    int shift = i * DISTANCE;
-                    points = createHexadedron(points, shift);
+                    points = addPoints("hexadedron", points, i);
+
                     for(int j = 0; j < 8; j++) {
                         indices.push_back(j + (i * 8));
                     }
@@ -77,11 +74,7 @@ namespace
 
                 //create roofs (pyramids)
                 for (int i = 0; i < numberOfHouses; i++) {
-                    int shift = i * DISTANCE;
-
-                    Tensor<double, 3> p8(0.5, 0.5 + shift, 1.5);
-
-                    points.push_back(p8);
+                    points = addPoints("pyramid", points, i);
 
                     for(int j = 4 + (i * 8); j < (8 + (i * 8)); j++) {
                         indices.push_back(j);
@@ -91,11 +84,7 @@ namespace
 
                 //create poles (lines)
                 for (int i = 0; i < numberOfHouses; i++) {
-                    int shift = i * DISTANCE;
-
-                    Tensor<double, 3> p9(0.5, 0.5 + shift, 1.7);
-
-                    points.push_back(p9);
+                    points = addPoints("line", points, i);
 
                     indices.push_back(8 * numberOfHouses + i);
                     indices.push_back(9 * numberOfHouses + i);
@@ -103,13 +92,7 @@ namespace
 
                 //create flags (triangles)
                 for (int i = 0; i < numberOfHouses; i++) {
-                    int shift = i * DISTANCE;
-
-                    Tensor<double, 3> p10(0.5, 0.5 + shift, 1.6);
-                    Tensor<double, 3> p11(0.5, 0.7 + shift, 1.66);
-
-                    points.push_back(p10);
-                    points.push_back(p11);
+                    points = addPoints("triangle", points, i);
 
                     indices.push_back(9 * numberOfHouses + i);
                     indices.push_back(points.size() - 2);
@@ -117,17 +100,11 @@ namespace
                 }
 
                 std::pair<Cell::Type, size_t> cellCounts[NUMBEROFCELLTYPES];
-                size_t houses = (size_t) numberOfHouses;
 
-                std::pair<Cell::Type, size_t> hexas(Cell::HEXAHEDRON, houses);
-                std::pair<Cell::Type, size_t> pyramids(Cell::PYRAMID, houses);
-                std::pair<Cell::Type, size_t> lines(Cell::LINE, houses);
-                std::pair<Cell::Type, size_t> triangles(Cell::TRIANGLE, houses);
-
-                cellCounts[0] = hexas;
-                cellCounts[1] = pyramids;
-                cellCounts[2] = lines;
-                cellCounts[3] = triangles;
+                cellCounts[0] = {Cell::HEXAHEDRON, numberOfHouses};
+                cellCounts[1] = {Cell::PYRAMID, numberOfHouses};
+                cellCounts[2] = {Cell::LINE, numberOfHouses};
+                cellCounts[3] = {Cell::TRIANGLE, numberOfHouses};
 
                 // create 3 dimensional Domain
                 std::shared_ptr< const DiscreteDomain< 3 > > myDomain = DomainFactory::makeDomainArbitrary(points, Precision::UINT64);
@@ -141,28 +118,52 @@ namespace
 
         private:
 
-            std::vector<Tensor<double, 3>> createHexadedron(std::vector<Tensor<double, 3>> points, int shift)
+            std::vector<Tensor<double, 3>> addPoints(std::string cellType, std::vector<Tensor<double, 3>> points, int houseIndex)
             {
-                // floor
-                Tensor<double, 3> p0(0.0, 0.0 + shift, 0.0);
-                Tensor<double, 3> p1(1.0, 0.0 + shift, 0.0);
-                Tensor<double, 3> p2(1.0, 1.0 + shift, 0.0);
-                Tensor<double, 3> p3(0.0, 1.0 + shift, 0.0);
+                //distance between the houses
+                double DISTANCE = 3.0;
 
-                // level
-                Tensor<double, 3> p4(0.0, 1.0 + shift, 1.0);
-                Tensor<double, 3> p5(1.0, 1.0 + shift, 1.0);
-                Tensor<double, 3> p6(1.0, 0.0 + shift, 1.0);
-                Tensor<double, 3> p7(0.0, 0.0 + shift, 1.0);
+                int shift = houseIndex * DISTANCE;
+                if (cellType == "hexadedron") {
+                    // floor
+                    Tensor<double, 3> p0(0.0, 0.0 + shift, 0.0);
+                    Tensor<double, 3> p1(1.0, 0.0 + shift, 0.0);
+                    Tensor<double, 3> p2(1.0, 1.0 + shift, 0.0);
+                    Tensor<double, 3> p3(0.0, 1.0 + shift, 0.0);
 
-                points.push_back(p0);
-                points.push_back(p1);
-                points.push_back(p2);
-                points.push_back(p3);
-                points.push_back(p4);
-                points.push_back(p5);
-                points.push_back(p6);
-                points.push_back(p7);
+                    // level
+                    Tensor<double, 3> p4(0.0, 1.0 + shift, 1.0);
+                    Tensor<double, 3> p5(1.0, 1.0 + shift, 1.0);
+                    Tensor<double, 3> p6(1.0, 0.0 + shift, 1.0);
+                    Tensor<double, 3> p7(0.0, 0.0 + shift, 1.0);
+                    points.push_back(p0);
+                    points.push_back(p1);
+                    points.push_back(p2);
+                    points.push_back(p3);
+                    points.push_back(p4);
+                    points.push_back(p5);
+                    points.push_back(p6);
+                    points.push_back(p7);
+                }
+
+                else if (cellType == "pyramid") {
+                    Tensor<double, 3> p8(0.5, 0.5 + shift, 1.5);
+                    points.push_back(p8);
+                }
+
+                else if (cellType == "line") {
+                    Tensor<double, 3> p9(0.5, 0.5 + shift, 1.7);
+                    points.push_back(p9);
+                }
+
+                else if (cellType == "triangle") {
+                    Tensor<double, 3> p10(0.5, 0.5 + shift, 1.6);
+                    Tensor<double, 3> p11(0.5, 0.7 + shift, 1.66);
+
+                    points.push_back(p10);
+                    points.push_back(p11);
+                }
+
                 return points;
             }
         };
